eed967505e8bfeac7775c24e8435b2b425be8b75
ercan-ozturk <ercanozturk788@gmail.com>
2025-08-08 14:08:39 +0300

    Input system via keyboard

--- sudoku.gd
+++ sudoku.gd
@@ -11,14 +11,13 @@ var matrix =  [
 	[0, 0, 0, 4, 1, 9, 0, 0, 5],
 	[0, 0, 0, 0, 8, 0, 0, 7, 9]
 ]
-var selectedButton
+var selectedButton: Vector2i
 
 const SIZE = 9
 func _ready():
-	print(checkIfValid(2, 1, 7))
 	populateGrid()
 
-func createButton(val, isInit):
+func createButton(val, isInit, pos:Vector2i):
 	if val == 0:
 		val = ""
 	var button = Button.new()
@@ -28,20 +27,21 @@ func createButton(val, isInit):
 		button.set("theme_override_colors/font_color", Color("yellow"))
 		
 	button.set("theme_override_font_sizes/font_size", 32)
-	button.custom_minimum_size = Vector2(62, 62)
-	button.pressed.connect(onButtonPressed)
+	button.custom_minimum_size = Vector2i(62, 62)
+	button.pressed.connect(onButtonPressed.bind(pos))
 	grid.add_child(button)
 	return button
-func onButtonPressed():
-	print("Button Pressed")
-	#selectedButton = 
+func onButtonPressed(pos:Vector2i):
+	print("Button Pressed: ", pos)
+	selectedButton = pos
+
 func populateGrid():
 	for i in range(SIZE):
 		for j in range(SIZE):
 			if matrix[i][j] == 0:
-				createButton(matrix[i][j], false)
+				createButton(matrix[i][j], false, Vector2i(i, j))
 			else:
-				createButton(matrix[i][j], true)
+				createButton(matrix[i][j], true, Vector2i(i, j))
 			
 func checkIfValid(row, col, num):
 	for i in range(SIZE):
@@ -57,5 +57,46 @@ func checkIfValid(row, col, num):
 		for j in range(SIZE/3):
 			if matrix[i+s_row][j+s_col] == num:
 				return false
+	changeNumber(row, col, num)
 	return true
+func changeNumber(row, col, num):
+	#$GridContainer. = str(num)
+	pass
+	
+func inputProcess(val):
+	if selectedButton:
+		if checkIfValid(selectedButton.x, selectedButton.y, val):
+			print("Valid")
+		else:
+			print("Invalid placement")
+func _input(event: InputEvent) -> void:
+	
+	if event.is_action_pressed("1"):
+
+		inputProcess(1)
+	if event.is_action_pressed("2"):
+		
+		inputProcess(2)
+	if event.is_action_pressed("3"):
+		
+		inputProcess(3)
+	if event.is_action_pressed("4"):
+		
+		inputProcess(4)
+	if event.is_action_pressed("5"):
+		
+		inputProcess(5)
+	if event.is_action_pressed("6"):
+		
+		inputProcess(6)
+	if event.is_action_pressed("7"):
+		
+		inputProcess(7)
+	if event.is_action_pressed("8"):
+		
+		inputProcess(8)
+	if event.is_action_pressed("9"):
+		
+		inputProcess(9)
+
 	
